9f567c043f2c0dd34b241ebdf772fb7baaf1abe9
Basic_python <arbazahmed7672>
2019-07-16 21:23:10 +0530

    commited

--- age_calculator.py
+++ age_calculator.py
@@ -29,5 +29,4 @@ if ww in range(0,5000):
     else:
         print("But you are under 18 year's")
 else:
-    print(f"you entered string(letters) but most probably you have to enter number(int or float)")
-
+    print(f"you entered string")
